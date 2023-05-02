0d317135e956e8e15f0ab68fa09be8382f84c7fa
Pfreym <spcompsys@gmail.com>
2023-05-02 10:09:57 +0800

    SideBar for cart

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,18 @@
+import { SidebarContext } from "../contexts/SidebarContext";
+import { useContext } from "react";
+import { BsBag } from "react-icons/bs";
+
 const Header = () => {
-  return <div>Header</div>;
+  const {isOpen,setIsOpen} = useContext(SidebarContext)
+  return (
+  <header className="bg-orange-500">
+    <div>Header</div>
+    <div onClick={()=> setIsOpen(!isOpen)}
+    className="cursor-pointer flex relative">
+      <BsBag className="text-2xl"/>
+    </div>
+  </header>
+  );
 };
 
 export default Header;
